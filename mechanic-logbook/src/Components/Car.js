9a7c60a0ae0d2675b38bf2baaa5dc2b8c2fedb7e
Andrew Coffey <121702632+AndrewCoffey02@users.noreply.github.com>
2023-12-19 13:32:31 +0000

    layout update

--- mechanic-logbook/src/Components/Car.js
+++ mechanic-logbook/src/Components/Car.js
@@ -5,7 +5,7 @@ export default function Car(car) {
     //return as array
     return car.myCars.map(
         (car) => {
-            return <Card myCar={car} key={car._id} Reload={() => {car.reloadData()}}></Card>
+            return <Card myCar={car} key={car._id} Reload={()=>{car.reloadData()}}></Card>
         } 
     );
 }
