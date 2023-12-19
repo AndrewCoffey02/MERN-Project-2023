4913c47b805435a02a3c10e46229c1fe4f163de3
Andrew Coffey <121702632+AndrewCoffey02@users.noreply.github.com>
2023-12-19 13:33:45 +0000

    added a delete Car method and button

--- mechanic-logbook/src/Components/Card.js
+++ mechanic-logbook/src/Components/Card.js
@@ -1,14 +1,38 @@
 import Card from 'react-bootstrap/Card';
-import  Button  from 'react-bootstrap/Button';
+import Button from 'react-bootstrap/Button';
+import Dropdown from 'react-bootstrap/Dropdown';
 import axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom';
 
 
 export default function Cards(car) {
 
+    const deleteCar = (e) => {
+        //ignore default function
+        e.preventDefault();
+        //http request to delete car from database
+        axios.delete('http://localhost:4000/api/cars/' + car.myCar._id)
+            .then((res) => {
+
+                // implements reload method
+                let Reload = car.Reload();
+            })
+            //request error
+            .catch(console.error());
+    }
+
+    const navigate = useNavigate();
+
+    const EditPage = (e) => {
+        e.preventDefault();
+        navigate('/editCar/'+car.myCar._id);
+    }
+
+
     return (
         //Car List display
-        <div align='center'>
-            <Card style= {{width: '36rem' }} className="mt-2" data-bs-theme="dark">
+        <div align="center"> 
+            <Card style={{ width: '36rem' }} className="mt-2" data-bs-theme="dark">
                 <Card.Header>Name: {car.myCar.owner}</Card.Header>
                 <Card.Body>
                     <Card.Subtitle className="mb-2 text-muted">Make: {car.myCar.make} </Card.Subtitle>
@@ -16,22 +40,17 @@ export default function Cards(car) {
                     <Card.Subtitle className="mb-2 text-muted">Year: {car.myCar.year}</Card.Subtitle>
                     <Card.Subtitle className="mb-2 text-muted">Engine: {car.myCar.engine}</Card.Subtitle>
                     <Card.Subtitle className="mb-2 text-muted">Problem: {car.myCar.problem}</Card.Subtitle>
+                    <Dropdown >
+                        <Dropdown.Toggle variant="secondary" id="dropdown-basic">options</Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            <Dropdown.Item onClick={deleteCar}>Delete</Dropdown.Item>
+                            <Dropdown.Item onClick={EditPage}>Edit</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                    <Link to={ '/editCar/'+car.myCar._id} className='btn btn-primary'>Edit</Link>
                 </Card.Body>
-                <Button variant="danger" classname="mb-2 text-muted" onClick={
-                    (e) => {
-                        //ignore default function
-                        e.preventDefault();
-                        //http request to delete car from database
-                        axios.delete('http://localhost:4000/api/cars/'+car.myCar._id)
-                        .then((res) => {
+                <Button variant="success" className="mb-2 text-muted">Mark as Finished</Button>
 
-                            // implements reload method
-                           let Reload = car.Reload();
-                        })
-                        //request error
-                        .catch(console.error());
-                    }
-                }>Finished</Button>
             </Card>
         </div>
     );
--- mechanic-logbook/src/Components/editCar.js
+++ mechanic-logbook/src/Components/editCar.js
@@ -1,20 +1,40 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 
-function Create() {
+function Edit() {
 
-    //create arrays for adding each statement into Log
-    const[Owner, setOwner] = useState([]);
-    const[Make, setMake] = useState([]);
-    const[Model, setModel] = useState([]);
-    const[Year, setYear] = useState([]);
-    const[Engine, setEngine] = useState([]);
-    const[Problem, setProblem] = useState([]);
+    let {id} = useParams();
 
-    //handles and adds to database
-    const handler = (e) => {
+    const[Owner, setOwner] = useState('');
+    const[Make, setMake] = useState('');
+    const[Model, setModel] = useState('');
+    const[Year, setYear] = useState('');
+    const[Engine, setEngine] = useState('');
+    const[Problem, setProblem] = useState('');
 
-        e.preventDefault();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+
+        axios.get('http://localhost:4000/api/cars/'+id)
+        .then((res) => {
+
+            setOwner(res.data.Owner);
+            setMake(res.data.Make);
+            setModel(res.data.Model);
+            setYear(res.data.Year);
+            setEngine(res.data.Engine);
+            setProblem(res.data.Problem);
+        })
+        .catch(
+            console.error()
+        )
+
+    },[])
+
+    const submitHandler = (e) => {
+        e.preventdefault();
 
         const car = {
             owner:Owner,
@@ -25,67 +45,68 @@ function Create() {
             problem:Problem
         }
 
-        //posts request through port
-        axios.post("http://localhost:4000/api/cars", car)
-        .then()
-        .catch();
-
+        axios.put("http://localhost:4000/api/cars/"+id, car)
+        .then((res) => {
+            navigate('/read');
+        })
+        .catch(console.error());
     }
     function clickMe() {
         alert("You have submitted to list");
     }
 
     return (
-        //Car card user layout
+        //Car card user layout but in an edit page
         <div align="center" >
-            <form onSubmit={handler}>
+            <h2>Edit file:</h2>
+            <form onSubmit={submitHandler}>
                 <div className="form-group" >
-                    <label>Please enter Car Owner: </label>
+                    <label>Please edit Car Owner: </label>
                     <input type="text"
                     className="form-control"
                     value={Owner} 
                     onChange={ (e) => { setOwner(e.target.value) }}/> 
                 </div> 
                 <div className="form-group">
-                    <label>Please enter Car Make: </label>
+                    <label>Edit car Make: </label>
                     <input type="text"
                     className="form-control"
                     value={Make}
                     onChange={ (e) => { setMake(e.target.value) }}/>
                 </div>
                 <div className="form-group">
-                    <label>Please enter Car Model: </label>
+                    <label>Edit car model:  </label>
                     <input type="text"
                     className="form-control"
                     value={Model}
                     onChange={ (e) => { setModel(e.target.value) }}/>
                 </div>
                 <div className="form-group">
-                    <label>Please enter what year the car was made: </label>
+                    <label>Edit car year: </label>
                     <input type="text"
                     className="form-control"
                     value={Year}
                     onChange={ (e) => { setYear(e.target.value) }}/>
                 </div>
                 <div className="form-group">
-                    <label>Please enter Car Engine size: </label>
+                    <label>Edit Car Engine size: </label>
                     <input type="text"
                     className="form-control"
                     value={Engine}
                     onChange={ (e) => { setEngine(e.target.value) }}/>
                 </div>
                 <div className="form-group">
-                    <label>Please enter the customers problem with the car: </label>
+                    <label>Edit car problem: </label>
                     <input type="text"
                     className="form-control"
                     value={Problem}
                     onChange={ (e) => { setProblem(e.target.value) }}/>
                 </div>
                 <input type="submit"
-                value="Add Car" onClick={clickMe}/>
+                value="Submit Edit" onClick={clickMe}/>
             </form>
         </div>
     )
-}
 
-export default Create;
+
+}
